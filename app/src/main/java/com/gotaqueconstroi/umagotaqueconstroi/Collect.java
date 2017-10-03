aa9f6d59a7e3cb332291003f28ef398864292411
apdiegosilva <32199467+apdiegosilva@users.noreply.github.com>
2017-10-03 15:33:33 -0300

    Tela de coleta semi pronta com eventos

--- app/src/main/java/com/gotaqueconstroi/umagotaqueconstroi/Collect.java
+++ app/src/main/java/com/gotaqueconstroi/umagotaqueconstroi/Collect.java
@@ -1,7 +1,14 @@
 package com.gotaqueconstroi.umagotaqueconstroi;
 
+import android.content.DialogInterface;
+import android.renderscript.Double2;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class Collect extends AppCompatActivity {
 
@@ -9,5 +16,45 @@ public class Collect extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_collect);
+        ImageButton btnColeta = (ImageButton) findViewById(R.id.btnColeta);
+        final EditText txtQuantidade = (EditText) findViewById(R.id.txtQuantidade);
+
+        btnColeta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Integer quantidade;
+                quantidade = Integer.parseInt(txtQuantidade.getText().toString());
+                //Criar Alert Dialog
+                AlertDialog.Builder dialog;
+                dialog = new AlertDialog.Builder(Collect.this);
+                //Titulo do Dialog
+                dialog.setTitle("Confirmação");
+                //Mensagem da Dialog
+                dialog.setMessage("Confirma a doação de "+quantidade+ " litros de óleo  ?");
+                //Não deixar cancelar a Dialog
+                dialog.setCancelable(false);
+                //Definindo ícone
+                dialog.setIcon(android.R.drawable.ic_dialog_alert);
+                //Botao Negativo
+                dialog.setNegativeButton("Não", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(Collect.this, "Clicou no NÃO", Toast.LENGTH_LONG).show();
+                    }
+                });
+                //Botao positivo
+                dialog.setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(Collect.this, "Clicou no Sim", Toast.LENGTH_LONG).show();
+                    }
+                });
+                //Criar a Dialog
+                dialog.create();
+                //Mostrar a Dialog
+                dialog.show();
+
+            }
+        });
     }
 }
--- app/src/main/java/com/gotaqueconstroi/umagotaqueconstroi/MainActivity.java
+++ app/src/main/java/com/gotaqueconstroi/umagotaqueconstroi/MainActivity.java
@@ -1,7 +1,10 @@
 package com.gotaqueconstroi.umagotaqueconstroi;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton btnColeta = (ImageButton) findViewById(R.id.btnColeta);
+        ImageButton btnComprar = (ImageButton) findViewById(R.id.btnComprar);
+        ImageButton btnHistorico = (ImageButton) findViewById(R.id.btnHistorico);
+        ImageButton btnTransparencia = (ImageButton) findViewById(R.id.btnTransparencia);
+
+        //Chamar tela coleta
+        btnColeta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,Collect.class));
+            }
+        });
+
     }
 }
