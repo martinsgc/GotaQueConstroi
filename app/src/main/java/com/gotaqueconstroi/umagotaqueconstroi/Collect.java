01f00f9eae844fc6a49e9fbc6bf1bde70118d649
martinsgc <32224222+martinsgc@users.noreply.github.com>
2017-10-24 07:12:13 -0200

    Criação de máscara na tela de solicitar coleta

--- app/src/main/java/com/gotaqueconstroi/umagotaqueconstroi/Collect.java
+++ app/src/main/java/com/gotaqueconstroi/umagotaqueconstroi/Collect.java
@@ -11,10 +11,59 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
 
 import java.sql.Time;
 
 public class Collect extends AppCompatActivity {
+
+
+    //MASK
+    public static abstract class Mask {
+        public static String unmask(String s) {
+            return s.replaceAll("[.]", "").replaceAll("[-]", "")
+                    .replaceAll("[/]", "").replaceAll("[(]", "")
+                    .replaceAll("[)]", "").replaceAll("[:]", "");
+        }
+
+        public static TextWatcher insert(final String mask, final EditText ediTxt) {
+            return new TextWatcher() {
+                boolean isUpdating;
+                String old = "";
+                public void onTextChanged(CharSequence s, int start, int before,int count) {
+                    String str = Mask.unmask(s.toString());
+                    String mascara = "";
+                    if (isUpdating) {
+                        old = str;
+                        isUpdating = false;
+                        return;
+                    }
+                    int i = 0;
+                    for (char m : mask.toCharArray()) {
+                        if (m != '#' && str.length() > old.length()) {
+                            mascara += m;
+                            continue;
+                        }
+                        try {
+                            mascara += str.charAt(i);
+                        } catch (Exception e) {
+                            break;
+                        }
+                        i++;
+                    }
+                    isUpdating = true;
+                    ediTxt.setText(mascara);
+                    ediTxt.setSelection(mascara.length());
+                }
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+                public void afterTextChanged(Editable s) {}
+            };
+        }
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +72,7 @@ public class Collect extends AppCompatActivity {
         ImageButton btnColeta = (ImageButton) findViewById(R.id.btnColeta);
         final EditText txtQuantidade = (EditText) findViewById(R.id.txtQuantidade);
         final EditText txtHorario = (EditText) findViewById(R.id.txtHorario);
+        txtHorario.addTextChangedListener(Mask.insert("##:##", txtHorario));
         ImageButton btnInformacao = (ImageButton) findViewById(R.id.btnInformacao);
 
         
