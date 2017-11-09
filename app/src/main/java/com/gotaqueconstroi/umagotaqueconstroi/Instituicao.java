c8ebbb50118bae8feb79400d59759668fd2f2665
martinsgc <32224222+martinsgc@users.noreply.github.com>
2017-11-09 07:02:47 -0200

    Alteração RadioButtons Instituições

--- app/src/main/java/com/gotaqueconstroi/umagotaqueconstroi/Instituicao.java
+++ app/src/main/java/com/gotaqueconstroi/umagotaqueconstroi/Instituicao.java
@@ -32,57 +32,56 @@ public class Instituicao extends AppCompatActivity {
                 String instituicao = null;
 
 
-                if (rdbApae.isSelected()) {
+                 if (rdbApae.isSelected()) {
                     instituicao = "APAE";
 
-                }
-                if (rdbAssociacao.isSelected()) {
+                } else if (rdbAssociacao.isSelected()) {
                     instituicao = "Associação de Bairro";
 
-                }
-                if (rdbCaps.isSelected()) {
+                } else if (rdbCaps.isSelected()) {
                     instituicao = "CAPS";
 
-                }
-                if (rdbCapsAd.isSelected()) {
+                } else if (rdbCapsAd.isSelected()) {
                     instituicao = "CAPS AD";
 
-                }
-
-                if (rdbApae.isSelected() || rdbAssociacao.isSelected() || rdbCaps.isSelected() || rdbCapsAd.isSelected()) {
-                    //Criar Alert Dialog
-                    AlertDialog.Builder dialog;
-                    dialog = new AlertDialog.Builder(Instituicao.this);
-                    //Titulo do Dialog
-                    dialog.setTitle("Confirmação");
-                    //Mensagem da Dialog
-                    dialog.setMessage("Confirma criação de vínculo com " + instituicao + "?");
-
-                    //Não deixar cancelar a Dialog
-                    dialog.setCancelable(false);
-                    //Definindo ícone
-                    dialog.setIcon(android.R.drawable.ic_dialog_alert);
-                    //Botao Negativo
-                    dialog.setNegativeButton("Não", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            Toast.makeText(Instituicao.this, "Caadastro não realizado!", Toast.LENGTH_LONG).show();
-                        }
-                    });
-                    //Botao positivo
-                    dialog.setPositiveButton("Sim", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            Toast.makeText(Instituicao.this, "Caadastro realizado com sucesso!", Toast.LENGTH_LONG).show();
-                            finish();
-                        }
-                    });
-                    //Criar a Dialog
-                    dialog.create();
-                    //Mostrar a Dialog
-                    dialog.show();
                 } else {
                     Toast.makeText(Instituicao.this, "Favor selecione uma instituição!", Toast.LENGTH_SHORT).show();
+
+                    if (rdbApae.isSelected() || rdbAssociacao.isSelected() || rdbCaps.isSelected() || rdbCapsAd.isSelected()) {
+                        //Criar Alert Dialog
+                        AlertDialog.Builder dialog;
+                        dialog = new AlertDialog.Builder(Instituicao.this);
+                        //Titulo do Dialog
+                        dialog.setTitle("Confirmação");
+                        //Mensagem da Dialog
+                        dialog.setMessage("Confirma criação de vínculo com " + instituicao + "?");
+
+                        //Não deixar cancelar a Dialog
+                        dialog.setCancelable(false);
+                        //Definindo ícone
+                        dialog.setIcon(android.R.drawable.ic_dialog_alert);
+                        //Botao Negativo
+                        dialog.setNegativeButton("Não", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(Instituicao.this, "Caadastro não realizado!", Toast.LENGTH_LONG).show();
+                            }
+                        });
+                        //Botao positivo
+                        dialog.setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(Instituicao.this, "Caadastro realizado com sucesso!", Toast.LENGTH_LONG).show();
+                                finish();
+                            }
+                        });
+                        //Criar a Dialog
+                        dialog.create();
+                        //Mostrar a Dialog
+                        dialog.show();
+
+
+                    }
                 }
 
             }
